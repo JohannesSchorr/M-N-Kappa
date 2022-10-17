cd085d157d4f480f34daa70ca757cbe58d2ce65c
Johannes Schorr <johannes.schorr@ke.uni-stuttgart.de>
2022-10-17 13:00:54 +0200

    [add] solver - tests

--- tests/unit/test_solver.py
+++ tests/unit/test_solver.py
@@ -1,10 +1,28 @@
 from unittest import TestCase, main
 
-# from m_n_kappa.solver import
+from m_n_kappa.solver import Newton, Bisection
+
+data = [
+    [0, 2],
+    [-5, -3],
+]
+
+
+class TestBisection(TestCase):
+    def setUp(self):
+        self.solver = Bisection(data=data, target=1, variable=0)
+
+    def test_compute(self):
+        self.assertEqual(self.solver.compute(), -2.5)
+
+
+class TestNewton(TestCase):
+    def setUp(self):
+        self.solver = Newton(data=data, target=1, variable=0)
+
+    def test_compute(self):
+        self.assertEqual(self.solver.compute(), -2.0)
 
 
 if __name__ == "__main__":
-    import sys
-
-    print(sys.path)
     main()
