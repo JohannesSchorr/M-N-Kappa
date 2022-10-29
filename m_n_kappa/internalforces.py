8c52d3271a9638ba6bec19baa2335dfdfdec0366
Johannes Schorr <johannes.schorr@ke.uni-stuttgart.de>
2022-10-29 16:00:32 +0200

    [refactor] changed design and remove unnecessary parts

--- m_n_kappa/internalforces.py
+++ m_n_kappa/internalforces.py
@@ -1,4 +1,5 @@
-from abc import ABC, abstractmethod, abstractproperty
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
 
 from .general import str_start_end
 
@@ -21,47 +22,107 @@ To Do
 """
 
 
-class ABCBeam(ABC):
-    @abstractproperty
-    def loading(self):
-        ...
-
-    @abstractmethod
-    def moment(self, at_position):
-        ...
-
-    @abstractmethod
-    def transversal_shear(self, at_position):
-        ...
-
-
 class ABCSingleSpan(ABC):
-    @abstractproperty
-    def loading(self):
+    """Meta class single span loading conditions shall be derived from"""
+
+    @property
+    @abstractmethod
+    def length(self) -> float:
+        """length of beam-span"""
+        ...
+
+    @property
+    @abstractmethod
+    def maximum_moment(self) -> float:
+        """maximum moment by the given load"""
+        ...
+
+    @property
+    @abstractmethod
+    def loading(self) -> float:
+        """sum of vertical loads"""
+        ...
+
+    @property
+    @abstractmethod
+    def transversal_shear_support_left(self) -> float:
+        """transversal shear value at the left support"""
+        ...
+
+    @property
+    @abstractmethod
+    def transversal_shear_support_right(self) -> float:
+        """transversal shear value at the right support"""
         ...
 
     @abstractmethod
-    def moment(self, at_position):
+    def moment(self, at_position: float) -> float:
+        """moment at the given position
+
+        Parameters
+        ----------
+        at_position : float
+            position where the moment needs to be computed at
+
+        Returns
+        -------
+        float
+            moment at the given position
+        """
         ...
 
     @abstractmethod
-    def transversal_shear(self, at_position):
+    def transversal_shear(self, at_position) -> float:
+        """transversal shear at the given position
+
+        Parameters
+        ----------
+        at_position : float
+            position where the transversal shear needs to be computed at
+
+        Returns
+        -------
+        float
+            transversal shear at the given position
+        """
         ...
 
-    @abstractproperty
-    def transversal_shear_support_left(self):
+    @abstractmethod
+    def position_of_maximum_moment(self) -> float:
+        """position of the maximum moment"""
         ...
 
-    @abstractproperty
-    def transversal_shear_support_right(self):
+    @abstractmethod
+    def load_by(self, moment: float, at_position: float):
         ...
+
+
+@dataclass
+class SingleLoad:
+    """container for single load"""
+
+    position_in_beam: float
+    value: float
+
+    def moment(self):
+        """moment by the single load (load x position_in_beam)"""
+        return self.position_in_beam * self.value
 
 
 class SingleSpan(ABCSingleSpan):
+    """ """
 
     __slots__ = "_length", "_uniform_load", "_loads", "_beam"
 
-    def __init__(self, length: list, loads: list = None, uniform_load: float = None):
+    def __init__(self, length: float, loads: list = None, uniform_load: float = None):
+        """
+
+        Parameters
+        ----------
+        length
+        loads
+        uniform_load
+        """
         self._length = length
         self._uniform_load = uniform_load
         self._loads = loads
@@ -75,44 +136,51 @@ class SingleSpan(ABCSingleSpan):
         return self._beam
 
     @property
-    def length(self):
+    def length(self) -> float:
         return self._length
 
     @property
-    def loading(self):
+    def loading(self) -> float:
         return self.beam.loading
 
     @property
-    def maximum_moment(self):
+    def maximum_moment(self) -> float:
         return self.beam.maximum_moment
 
     @property
-    def transversal_shear_support_left(self):
+    def transversal_shear_support_left(self) -> float:
         return self.beam.transversal_shear_support_left
 
     @property
-    def transversal_shear_support_right(self):
+    def transversal_shear_support_right(self) -> float:
         return self.beam.transversal_shear_support_right
 
-    def moment(self, at_position):
+    def moment(self, at_position) -> float:
         return self.beam.moment(at_position)
 
-    def transversal_shear(self, at_position):
+    def transversal_shear(self, at_position) -> float:
         return self.beam.transversal_shear(at_position)
 
-    def _is_uniform_load(self):
+    def position_of_maximum_moment(self) -> float:
+        return self.beam.position_of_maximum_moment()
+
+    def load_by(self, moment: float, at_position: float) -> float:
+        """load that leads to given moment at the given position"""
+        return self.beam.load_by(moment, at_position)
+
+    def _is_uniform_load(self) -> bool:
         if self._uniform_load is not None:
             return True
         else:
             return False
 
-    def _is_single_loads(self):
+    def _is_single_loads(self) -> bool:
         if self._loads is not None:
             return True
         else:
             return False
 
-    def __get_beam_class(self):
+    def __get_beam_class(self) -> ABCSingleSpan:
         if self._is_uniform_load():
             return SingleSpanUniformLoad(self.length, self._uniform_load)
         elif self._is_single_loads():
@@ -121,16 +189,36 @@ class SingleSpan(ABCSingleSpan):
             raise ValueError()
 
 
+@dataclass
+class Moment:
+    """container for moment at position"""
+
+    position_in_beam: float
+    value: float
+
+
 class SingleSpanSingleLoads(ABCSingleSpan):
+    """single span with single load(s)"""
 
     __slots__ = "_length", "_loads"
 
-    def __init__(self, length: float, loads: list):
+    def __init__(self, length: float, loads: list[SingleLoad]):
+        """
+        Parameters
+        ----------
+        length : float
+            length of the beam-span
+        loads : list[SingleLoad]
+            loads applied to the beam
+        """
         self._length = length
         self._loads = loads
 
+    def __repr__(self) -> str:
+        return f"SingleSpanSingleLoads(length={self.length}, loads={self.loads})"
+
     @str_start_end
-    def __str__(self):
+    def __str__(self) -> str:
         text = [
             "Single span with single loads",
             "-----------------------------",
@@ -139,83 +227,134 @@ class SingleSpanSingleLoads(ABCSingleSpan):
             "----------",
             f"length = {self.length}",
             f"loads = {self.loads} | loading = {self.loading}",
-            f"transversal shear: column left = {self.transversal_shear_support_left} | column right = {self.transversal_shear_support_right}",
+            f"transversal shear: column left = {self.transversal_shear_support_left} |"
+            + " column right = {self.transversal_shear_support_right}",
             f"Maximum Moment = {self.maximum_moment}",
         ]
         return "\n".join(text)
 
     @property
-    def length(self):
+    def length(self) -> float:
         return self._length
 
     @property
-    def loads(self) -> list:
+    def loads(self) -> list[SingleLoad]:
         return self._loads
 
     @property
-    def maximum_moment(self):
-        return self._maximum_moment()
+    def maximum_moment(self) -> float:
+        return self._maximum_moment_value()
 
     @property
-    def transversal_shear_support_left(self):
+    def transversal_shear_support_left(self) -> float:
         return self._loading() + self._column_right()
 
     @property
-    def transversal_shear_support_right(self):
+    def transversal_shear_support_right(self) -> float:
         return self._column_right()
 
     @property
-    def loading(self):
+    def loading(self) -> float:
         return self._loading()
 
-    def moment(self, at_position):
+    def moment(self, at_position) -> float:
         return self._moment(at_position)
 
-    def transversal_shear(self, at_position):
+    def position_of_maximum_moment(self) -> float:
+        return self._maximum_moment().position_in_beam
+
+    def transversal_shear(self, at_position) -> float:
         return self._transversal_shear(at_position)
 
-    def _column_right(self):
-        return (-1) * self._transversal_load() / self.length
+    def load_by(self, moment: float, at_position: float):
+        pass  # TODO
 
-    def _transversal_load(self):
-        return sum([load[0] * load[1] for load in self.loads])
+    def load_by_factor(self, moment: float, at_position):
+        moment_1 = self.moment(at_position)
+        moment_2 = self.single_span_with_factor(10.0).moment(at_position)
+        factor = 1.0 + (moment - moment_1) * (10.0 - 1.0) / (moment_2 - moment_1)
+        return self.single_span_with_factor(factor)
 
-    def _loading(self):
+    def single_span_with_factor(self, factor: float):
+        return SingleSpanSingleLoads(self.length, self.single_loads_with_factor(factor))
+
+    def single_span_with_value(self, value: float):
+        return SingleSpanSingleLoads(self.length, self.single_loads_with_value(value))
+
+    def single_loads_with_factor(self, factor: float) -> list[SingleLoad]:
+        """single loads multiplied with the given factor"""
+        return [
+            SingleLoad(load.position_in_beam, load.value * factor)
+            for load in self.loads
+        ]
+
+    def single_loads_with_value(self, value: float) -> list[SingleLoad]:
+        """single loads plus given value"""
+        return [
+            SingleLoad(load.position_in_beam, load.value + value) for load in self.loads
+        ]
+
+    def _column_right(self) -> float:
+        return (-1.0) * self._load_moments() / self.length
+
+    def _load_moments(self) -> float:
+        return sum([load.moment() for load in self.loads])
+
+    def _loading(self) -> float:
         return sum(self._single_loads())
 
-    def _moment(self, at_position: float):
+    def _moment(self, at_position: float) -> float:
         moment = self.transversal_shear_support_left * at_position
         for load in self.loads:
-            if load[0] < at_position:
-                moment -= self.moment_by(load)
+            if load.position_in_beam < at_position:
+                moment -= load.moment()
         return moment
 
-    def moment_by(self, load: list, at_position: float):
-        return (at_position - load[0]) * load[1]
+    def moment_by(self, load: SingleLoad, at_position: float) -> float:
+        return (at_position - load.position_in_beam) * load.value
 
-    def _moments(self):
-        return [[load[0], self._moment(load[0])] for load in self.loads]
+    def _moments(self) -> list[Moment]:
+        return [
+            Moment(load.position_in_beam, self._moment(load.position_in_beam))
+            for load in self.loads
+        ]
 
-    def _maximum_moment(self):
+    def _maximum_moment(self) -> Moment:
         moments = self._moments()
-        moments.sort(key=lambda x: x[1], reverse=True)
-        return moments[0][1]
+        moments.sort(key=lambda x: x.value, reverse=True)
+        return moments[0]
 
-    def _single_loads(self) -> list:
-        return [load[1] for load in self.loads]
+    def _maximum_moment_value(self) -> float:
+        return self._maximum_moment().value
 
-    def _transversal_shear(self, at_position):
+    def _single_loads(self) -> list[float]:
+        return [load.value for load in self.loads]
+
+    def _transversal_shear(self, at_position: float) -> float:
         shear = self.transversal_shear_support_left
-        for load in self._single_loads():
-            if load[0] < at_position:
-                shear -= load[1]
+        for load in self.loads:
+            if load.position_in_beam < at_position:
+                shear -= load.value
         return shear
 
 
 class SingleSpanUniformLoad(ABCSingleSpan):
+    """single span with uniform load"""
+
     def __init__(self, length: float, load: float):
+        """
+        Parameters
+        ----------
+        length : float
+            length of the beam-span
+        load : float
+            line-load the beam is uniformly loaded with
+        """
         self._length = length
         self._load = load
+
+    def __repr__(self) -> str:
+        return f"SingleSpanUniformLoad(length={self.length}, load={self.load})"
 
     @str_start_end
     def __str__(self):
@@ -227,21 +366,25 @@ class SingleSpanUniformLoad(ABCSingleSpan):
             "----------",
             f"length = {self.length}",
             f"load = {self.load} | loading = {self.loading}",
-            f"transversal shear: column left = {self.transversal_shear_support_left} | column right = {self.transversal_shear_support_right}",
+            f"transversal shear: column left = {self.transversal_shear_support_left} | "
+            + "column right = {self.transversal_shear_support_right}",
             f"Maximum Moment = {self.maximum_moment}",
         ]
         return "\n".join(text)
 
     @property
-    def length(self):
+    def length(self) -> float:
+        """length of the beam span"""
         return self._length
 
     @property
-    def load(self):
+    def load(self) -> float:
+        """line load the beam is loaded with"""
         return self._load
 
     @property
     def loading(self):
+        """total load of the beam span"""
         return self._loading()
 
     @property
@@ -264,7 +407,11 @@ class SingleSpanUniformLoad(ABCSingleSpan):
 
     def load_by_moment(self, maximum_moment) -> float:
         """compute load by given maximum moment"""
-        return maximum_moment * 8.0 / (self.length ** (2.0))
+        return maximum_moment * 8.0 / (self.length ** 2.0)
+
+    def load_by(self, moment: float, at_position: float) -> ABCSingleSpan:
+        load = moment / (0.5 * (self.length * at_position - at_position ** 2.0))
+        return SingleSpanUniformLoad(self.length, load)
 
     def position_of_maximum_moment(self) -> float:
         """position where the moment is the maximum"""
@@ -279,23 +426,11 @@ class SingleSpanUniformLoad(ABCSingleSpan):
     def _moment(self, at_position: float):
         return (
             self._support_transversal_shear() * at_position
-            - 0.5 * self.load * at_position ** (2.0)
+            - 0.5 * self.load * at_position ** 2.0
         )
 
     def _maximum_moment(self):
-        return self.load * self.length ** (2.0) / 8.0
+        return self.load * self.length ** 2.0 / 8.0
 
     def _transversal_shear(self, at_position):
         return self._support_transversal_shear() - self.load * at_position
-
-
-if __name__ == "__main__":
-
-    # beam = SingleSpanUniformLoad(10.0, 3.0)
-    # print(beam)
-
-    # beam = SingleSpanSingleLoads(10.0, [[5.0, 10.0], [7.0, 10.0]])
-    # print(beam)
-
-    beam = SingleSpan(length=10.0, loads=[[5.0, 10.0], [7.0, 10.0]])
-    print(beam)
