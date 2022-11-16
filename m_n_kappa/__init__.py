17985ad09e1d36b0c66bff0a9ec4a18615ef8620
Johannes Schorr <johannes.schorr@ke.uni-stuttgart.de>
2022-11-16 09:07:36 +0100

    [add] __init__ - interface-classes

---
+++ m_n_kappa/__init__.py
@@ -0,0 +1,10 @@
+from .material import Concrete, Steel, Reinforcement
+from .geometry import Rectangle, Trapezoid
+from .crosssection import Crosssection
+from .deformation import Beam
+from .internalforces import (
+    SingleSpanUniformLoad,
+    SingleLoad,
+    SingleSpanSingleLoads,
+    SingleSpan
+)
