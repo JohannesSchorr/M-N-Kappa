8ad113a97f6531c7766ab2cc54d977f6150c7ccc
Johannes Schorr <johannes.schorr@ke.uni-stuttgart.de>
2022-11-19 13:38:46 +0100

    [update] deformation.py
    Class Beam:
    - metod: 'deformation' - switch case loading == 0.0 from method 'deformations'

--- m_n_kappa/deformation.py
+++ m_n_kappa/deformation.py
@@ -7,7 +7,7 @@ from .internalforces import (
     ABCSingleSpan,
     SingleSpanSingleLoads,
     SingleLoad,
-    SingleSpanUniformLoad
+    SingleSpanUniformLoad,
 )
 
 
@@ -18,7 +18,7 @@ class Node:
     stores the cross-section, the position_value in the beam and the computed M-Kappa-Curve
     """
 
-    __slots__ = '_cross_section', '_m_kappa_curve', '_position', '_number'
+    __slots__ = "_cross_section", "_m_kappa_curve", "_position", "_number"
 
     node_number: int = 0
 
@@ -26,8 +26,7 @@ class Node:
         self._cross_section = cross_section
         self._position = position
         self._m_kappa_curve = MKappaCurve(
-            self.cross_section,
-            include_positive_curvature=True
+            self.cross_section, include_positive_curvature=True
         ).m_kappa_points
         Node.node_number += 1
         self._number = Node.node_number
@@ -52,7 +51,9 @@ class Node:
         """compute curvature given a moment using the M-Kappa-Curve"""
         return self.m_kappa_curve.curvature(moment)
 
-    def incremental_deformation(self, load: ABCSingleSpan, single_load: SingleSpanSingleLoads) -> float:
+    def incremental_deformation(
+        self, load: ABCSingleSpan, single_load: SingleSpanSingleLoads
+    ) -> float:
         """determine curvature and single-load-moment at the node under given loading"""
         moment = load.moment(self.position)
         curvature = self.curvature_by(moment)
@@ -95,19 +96,22 @@ class Loading:
 
     def load_steps(self) -> list[LoadStep]:
         return [
-            LoadStep(self.load.load_by(point.moment, self._position_maximum_moment()), point)
+            LoadStep(
+                self.load.load_by(point.moment, self._position_maximum_moment()), point
+            )
             for point in self.decisive_m_kappa_curve().points
         ]
 
     def m_kappa_curve_at(self, position) -> MKappaCurvePoints:
-        return list(
-            filter(lambda x: x.position == position, self.nodes)
-        )[0].m_kappa_curve
+        return list(filter(lambda x: x.position == position, self.nodes))[
+            0
+        ].m_kappa_curve
 
 
 @dataclass
 class Deformation:
     """container for deformations"""
+
     position: float
     load: float
     deformation: float
@@ -119,14 +123,14 @@ class Beam:
     """Beam consisting of nodes"""
 
     __slots__ = (
-        '_cross_section',
-        '_length',
-        '_element_number',
-        '_load',
-        '_element_standard_length',
-        '_positions',
-        '_nodes',
-        '_load_steps'
+        "_cross_section",
+        "_length",
+        "_element_number",
+        "_load",
+        "_element_standard_length",
+        "_positions",
+        "_nodes",
+        "_load_steps",
     )
 
     def __init__(
@@ -134,7 +138,7 @@ class Beam:
         cross_section: Crosssection,
         length: float,
         element_number: int,
-        load: ABCSingleSpan
+        load: ABCSingleSpan,
     ):
         """
         Parameters
@@ -204,6 +208,8 @@ class Beam:
         float
             deformation under given load at given position_value
         """
+        if load.loading == 0.0:
+            return 0.0
         if self._position_in_beam(at_position):
             if at_position in self.positions:
                 return self._deformation_at_node(at_position, load)
@@ -214,10 +220,7 @@ class Beam:
         """computes deformations at given position_value for relevant load-steps"""
         deformations = []
         for load_step in self.load_steps:
-            if load_step.load.loading == 0.0:
-                computed_deformation = 0.0
-            else:
-                computed_deformation = self.deformation(at_position, load_step.load)
+            computed_deformation = self.deformation(at_position, load_step.load)
             deformations.append(
                 Deformation(
                     position=at_position,
@@ -237,11 +240,17 @@ class Beam:
     def _deformation_at_node(self, at_position: float, load: ABCSingleSpan) -> float:
         return sum(self._incremental_deformations(at_position, load))
 
-    def _deformation_between_nodes(self, at_position: float, load: ABCSingleSpan) -> float:
+    def _deformation_between_nodes(
+        self, at_position: float, load: ABCSingleSpan
+    ) -> float:
         smaller_position = max(list(filter(lambda x: x < at_position, self.positions)))
-        deformation_at_smaller_position = self._deformation_at_node(smaller_position, load)
+        deformation_at_smaller_position = self._deformation_at_node(
+            smaller_position, load
+        )
         higher_position = min(list(filter(lambda x: x > at_position, self.positions)))
-        deformation_at_higher_position = self._deformation_at_node(higher_position, load)
+        deformation_at_higher_position = self._deformation_at_node(
+            higher_position, load
+        )
         return interpolation(
             position_value=at_position,
             first_pair=[smaller_position, deformation_at_smaller_position],
@@ -249,28 +258,31 @@ class Beam:
         )
 
     def _incremental_deformations(
-        self,
-        for_position: float,
-        load: ABCSingleSpan
+        self, for_position: float, load: ABCSingleSpan
     ) -> list[float]:
         single_load = SingleLoad(for_position, 1.0)
         single_span_single_load = SingleSpanSingleLoads(self.length, [single_load])
         return [
             self._incremental_deformation(node_index, load, single_span_single_load)
-            for node_index in range(len(self.nodes) - 1)]
+            for node_index in range(len(self.nodes) - 1)
+        ]
 
     def _incremental_deformation(
-        self,
-        node_index: int,
-        load: ABCSingleSpan,
-        single_load: SingleSpanSingleLoads
+        self, node_index: int, load: ABCSingleSpan, single_load: SingleSpanSingleLoads
     ):
-        node_1_result = self.nodes[node_index].incremental_deformation(load, single_load)
-        node_2_result = self.nodes[node_index+1].incremental_deformation(load, single_load)
-        return 0.5*(node_1_result + node_2_result) * self._element_length(node_index)
+        node_1_result = self.nodes[node_index].incremental_deformation(
+            load, single_load
+        )
+        node_2_result = self.nodes[node_index + 1].incremental_deformation(
+            load, single_load
+        )
+        return 0.5 * (node_1_result + node_2_result) * self._element_length(node_index)
 
     def _create_positions(self) -> list[float]:
-        positions = [number * self.element_standard_length for number in range(1, self.element_number)]
+        positions = [
+            number * self.element_standard_length
+            for number in range(1, self.element_number)
+        ]
         positions.append(self.load.position_of_maximum_moment())
         positions = list(set(positions))
         positions = sorted(positions)
@@ -286,4 +298,4 @@ class Beam:
         return [Node(self.cross_section, position) for position in self.positions]
 
     def _element_length(self, node_index) -> float:
-        return self.nodes[node_index+1].position - self.nodes[node_index].position
+        return self.nodes[node_index + 1].position - self.nodes[node_index].position
