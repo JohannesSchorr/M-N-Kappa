4d24cb7093cdfb59dd56105adeb3fc1a44c906da
Johannes Schorr <johannes.schorr@ke.uni-stuttgart.de>
2022-11-19 13:33:34 +0100

    [update] __init__.py
    - Composed geometries: UPEProfile, RebarLayer, IProfile
    - Geometry: Circle

--- m_n_kappa/__init__.py
+++ m_n_kappa/__init__.py
@@ -1,10 +1,10 @@
 from .material import Concrete, Steel, Reinforcement
-from .geometry import Rectangle, Trapezoid
+from .geometry import Rectangle, Trapezoid, UPEProfile, RebarLayer, IProfile, Circle
 from .crosssection import Crosssection
 from .deformation import Beam
 from .internalforces import (
     SingleSpanUniformLoad,
     SingleLoad,
     SingleSpanSingleLoads,
-    SingleSpan
+    SingleSpan,
 )
