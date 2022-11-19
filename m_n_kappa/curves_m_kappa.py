752b952eda8233fa3807cf04eeb103817cf09f34
Johannes Schorr <johannes.schorr@ke.uni-stuttgart.de>
2022-11-19 09:15:46 +0100

    [update] curves_m_kappa
    - implemented class StrainPosition in Workflow
    - fixed typos
    - added and fixed documentation

--- m_n_kappa/curves_m_kappa.py
+++ m_n_kappa/curves_m_kappa.py
@@ -1,17 +1,17 @@
 from dataclasses import dataclass
-from .crosssection import (
-    BoundaryValues,
-    Crosssection,
-    Boundaries,
-)
+
+from .crosssection import Crosssection
+from .curvature_boundaries import BoundaryValues,Boundaries
+
 from .general import (
     interpolation,
     print_sections,
     print_chapter,
     str_start_end,
+    StrainPosition
 )
 from .points import MKappaByStrainPosition
-
+from .solver import Newton, Bisection
 
 @dataclass
 class MKappaCurvePoint:
@@ -20,10 +20,8 @@ class MKappaCurvePoint:
     moment: float
     curvature: float
     neutral_axis: float
-    crosssection: Crosssection
-    strain: float
-    position: float
-    material: str
+    cross_section: Crosssection
+    strain_position: StrainPosition
 
     def __eq__(self, other) -> bool:
         if not isinstance(other, MKappaCurvePoint):
@@ -37,9 +35,13 @@ class MKappaCurvePoint:
         else:
             return False
 
-    def moment_curvature(self) -> list:
-        """list of moment and curvature"""
+    def moment_curvature(self) -> list[float]:
+        """pair of moment and curvature"""
         return [self.moment, self.curvature]
+
+    def curvature_moment(self) -> list[float]:
+        """pair of curvature and moment"""
+        return [self.curvature, self.moment]
 
 
 class MKappaCurvePoints:
@@ -50,6 +52,10 @@ class MKappaCurvePoints:
         self._points = []
 
     @property
+    def moments(self) -> list[float]:
+        return [point.moment for point in self.points]
+
+    @property
     def points(self) -> list[MKappaCurvePoint]:
         return self._points
 
@@ -58,10 +64,8 @@ class MKappaCurvePoints:
         moment: float,
         curvature: float,
         neutral_axis: float,
-        crosssection: MKappaByStrainPosition,
-        strain: float,
-        position: float,
-        material: str,
+        cross_section: MKappaByStrainPosition,
+        strain_position: StrainPosition,
     ) -> None:
         """add moment-curvature point to list
 
@@ -73,23 +77,17 @@ class MKappaCurvePoints:
             curvature of the point
         neutral_axis : float
             neutral-axis leading to equilibrium (after variation)
-        crosssection : MKappaByStrainPosition
+        cross_section : MKappaByStrainPosition
             computed cross-section
-        strain : float
-            strain-value
-        position : float
-            position where strain-value is given
-        material : str
-            material the strain-value is given in
+        strain_position : StrainPosition
+            strain and its position
         """
         point = MKappaCurvePoint(
                 moment,
                 curvature,
                 neutral_axis,
-                crosssection,
-                strain,
-                position,
-                material,
+                cross_section,
+                strain_position,
             )
         if point not in self.points:
             self._points.append(point)
@@ -97,27 +95,31 @@ class MKappaCurvePoints:
 
     def curvature(self, by_moment: float) -> float:
         """get curvature at given moment"""
+        if self.maximum_moment() < by_moment:
+            raise ValueError(
+                f"{by_moment=} > maximum moment in the curve {self.maximum_moment()}"
+                f"Therefore, no matching moment value could be found."
+            )
         point_index = self._determine_index(by_moment)
         return interpolation(
             by_moment,
-            self._moment_curvature(point_index),
-            self._moment_curvature(point_index + 1),
+            self._curvature_moment(point_index),
+            self._curvature_moment(point_index + 1),
         )
 
     def _moment_curvature(self, by_index) -> list[float]:
         return self.points[by_index].moment_curvature()
 
-    def _determine_index(self, moment):
+    def _curvature_moment(self, by_index) -> list[float]:
+        return self.points[by_index].curvature_moment()
+
+    def _determine_index(self, moment: float) -> int:
         for index in range(len(self.points) - 1):
             if self.points[index].moment <= moment <= self.points[index + 1].moment:
                 return index
 
     def _sort_points_by_curvature(self):
         self.points.sort(key=lambda x: x.curvature)
-
-    @property
-    def moments(self) -> list[float]:
-        return [point.moment for point in self.points]
 
     def maximum_moment(self) -> float:
         return max(self.moments)
@@ -131,9 +133,8 @@ class MKappaCurveCurvature:
         "_cross_section",
         "_maximum_curvature",
         "_minimum_curvature",
-        "_start_strain",
-        "_start_position",
         "_m_kappa_failure",
+        "_start_strain_position"
     )
 
     def __init__(
@@ -141,14 +142,12 @@ class MKappaCurveCurvature:
         cross_section: Crosssection,
         maximum_curvature: float,
         minimum_curvature: float,
-        start_strain: float,
-        start_position: float,
+        start_strain_position: StrainPosition,
     ):
         self._cross_section = cross_section
         self._maximum_curvature = maximum_curvature
         self._minimum_curvature = minimum_curvature
-        self._start_strain = start_strain
-        self._start_position = start_position
+        self._start_strain_position = start_strain_position
         self._m_kappa_failure = self._get_m_kappa_failure()
 
     @property
@@ -164,19 +163,15 @@ class MKappaCurveCurvature:
         return self._minimum_curvature
 
     @property
-    def start_strain(self) -> float:
-        return self._start_strain
-
-    @property
-    def start_position(self) -> float:
-        return self._start_position
+    def start_strain_position(self) -> StrainPosition:
+        return self._start_strain_position
 
     @property
     def m_kappa_failure(self):
         """Moment and curvature at failure of cross-section"""
         return self._m_kappa_failure
 
-    def get_material_points_inside_curvature(self) -> list:
+    def get_material_points_inside_curvature(self) -> list[StrainPosition]:
         """material points between zero curvature and failure of the cross-section"""
         return (
             self.m_kappa_failure.computed_cross_section.get_material_points_inside_curvature()
@@ -184,30 +179,27 @@ class MKappaCurveCurvature:
 
     def _get_m_kappa_failure(self):
         return self._m_kappa(
-            strain_position=self.start_position,
-            strain_at_position=self.start_strain,
+            strain_position=self.start_strain_position,
             maximum_curvature=self.maximum_curvature,
             minimum_curvature=self.minimum_curvature,
         )
 
     def _m_kappa(
         self,
-        strain_position: float,
-        strain_at_position: float,
+        strain_position: StrainPosition,
         maximum_curvature: float,
         minimum_curvature: float,
     ) -> MKappaByStrainPosition:
         return MKappaByStrainPosition(
             self.cross_section,
             strain_position=strain_position,
-            strain_at_position=strain_at_position,
             maximum_curvature=maximum_curvature,
             minimum_curvature=minimum_curvature,
         )
 
 
 class MKappaCurve:
-    """computation of a M-Kappa-Curve assuming full interaction"""
+    """computation of M-Kappa-Curve assuming full interaction"""
 
     __slots__ = (
         "_cross_section",
@@ -231,11 +223,11 @@ class MKappaCurve:
         Parameters
         ----------
         cross_section : Crosssection
-                cross section to compute
+                cross-section to compute
         include_positive_curvature : bool
                 if True than positive curvature values are computed
         include_negative_curvature : bool
-                if True than negative curvatrue values are computed
+                if True than negative curvature values are computed
         """
         self._cross_section = cross_section
         self._include_positive_curvature = include_positive_curvature
@@ -262,12 +254,13 @@ class MKappaCurve:
         return print_chapter(text)
 
     def _print_title(self) -> str:
-        return print_sections(["MKappaCurve", len("MKappaCurve") * "="])
+        class_name = self.__class__.__name__
+        return print_sections([class_name, len(class_name) * "="])
 
     def _print_initialization(self) -> str:
         return print_chapter(["Initialization", "--------------", self.__repr__()])
 
-    def _print_m_kappa_points(self):
+    def _print_m_kappa_points(self) -> str:
         text = [
             "M-Kappa-Points",
             "--------------",
@@ -283,9 +276,9 @@ class MKappaCurve:
                     point.moment,
                     point.curvature,
                     point.neutral_axis,
-                    point.strain,
-                    point.position,
-                    point.material,
+                    point.strain_position.strain,
+                    point.strain_position.position,
+                    point.strain_position.material,
                 )
             )
         text.append(
@@ -293,7 +286,8 @@ class MKappaCurve:
         )
         return print_sections(text)
 
-    def _print_legend(self):
+    @staticmethod
+    def _print_legend() -> str:
         text = [
             "Legend",
             "------",
@@ -302,8 +296,8 @@ class MKappaCurve:
             "\t"
             + 'Curvature:    curvature resulting from the "Neutral axis" and the "Strain" at "Position"',
             "\t" + "Neutral axis: computed neutral axis",
-            "\t" + 'Strain:       starting strain at "Position"',
-            "\t" + 'Position:     starting position for the "Strain"',
+            "\t" + 'Strain:       starting strain_value at "Position"',
+            "\t" + 'Position:     starting position_value for the "Strain"',
             "\t"
             + 'Material:     material where "Strain" and "Position" are taken from',
         ]
@@ -344,24 +338,18 @@ class MKappaCurve:
             cross_section=self.cross_section,
             maximum_curvature=boundary.maximum_curvature.curvature,
             minimum_curvature=boundary.minimum_curvature.compute(
-                boundary.maximum_curvature.start.strain,
-                boundary.maximum_curvature.start.position,
+                boundary.maximum_curvature.start
             ),
-            start_strain=boundary.maximum_curvature.start.strain,
-            start_position=boundary.maximum_curvature.start.position,
+            start_strain_position=boundary.maximum_curvature.start,
         )
 
     def _get_positive_m_kappa_curve_curvature(self) -> MKappaCurveCurvature:
         if self.include_positive_curvature:
             return self._get_m_kappa_curve_curvature(self.boundaries.positive)
-        else:
-            return None
 
     def _get_negative_m_kappa_curve_curvature(self) -> MKappaCurveCurvature:
         if self.include_negative_curvature:
             return self._get_m_kappa_curve_curvature(self.boundaries.negative)
-        else:
-            return None
 
     def _compute_negative_curvature_failure(self) -> None:
         m_kappa = self.negative.m_kappa_failure
@@ -369,15 +357,12 @@ class MKappaCurve:
 
     def _compute_negative_curvature_intermediate(self) -> None:
         for strain_position in self.negative.get_material_points_inside_curvature():
-            strain = strain_position["strain"]
-            position = strain_position["position"]
             m_kappa = self._m_kappa(
-                position_strain=position,
-                strain_at_position=strain,
-                maximum_curvature=self._maximum_negative_curvature(strain, position),
-                minimum_curvature=self._minimum_negative_curvature(strain, position),
+                position_strain=strain_position,
+                maximum_curvature=self._maximum_negative_curvature(strain_position),
+                minimum_curvature=self._minimum_negative_curvature(strain_position),
             )
-            self._compute_values(m_kappa, strain_position["material"])
+            self._compute_values(m_kappa)
 
     def _compute_negative_curvature_values(self) -> None:
         if self.negative is not None:
@@ -386,31 +371,45 @@ class MKappaCurve:
 
     def _compute_positive_curvature_failure(self) -> None:
         m_kappa = self.positive.m_kappa_failure
-        self._compute_values(m_kappa, "Concrete")
+        self._compute_values(m_kappa)
 
     def _compute_positive_curvature_intermediate(self) -> None:
         for strain_position in self.positive.get_material_points_inside_curvature():
-            strain = strain_position["strain"]
-            position = strain_position["position"]
             m_kappa = self._m_kappa(
-                position_strain=position,
-                strain_at_position=strain,
-                maximum_curvature=self._maximum_positive_curvature(strain, position),
-                minimum_curvature=self._minimum_positive_curvature(strain, position),
+                position_strain=strain_position,
+                maximum_curvature=self._maximum_positive_curvature(strain_position),
+                minimum_curvature=self._minimum_positive_curvature(strain_position),
             )
-            self._compute_values(m_kappa, strain_position["material"])
+            if not m_kappa.successful:
+                m_kappa = MKappaByStrainPosition(
+                    self.cross_section,
+                    strain_position=strain_position,
+                    maximum_curvature=self._maximum_positive_curvature(strain_position),
+                    minimum_curvature=self._minimum_positive_curvature(strain_position),
+                    maximum_iterations=100,
+                    solver=Bisection,
+                )
+            self._compute_values(m_kappa)
 
-    def _maximum_positive_curvature(self, strain: float, position: float) -> float:
-        return self.boundaries.positive.maximum_curvature.compute(strain, position)
+    def _maximum_positive_curvature(self, strain_position: StrainPosition) -> float:
+        """compute the maximum possible positive curvature given the strain_value
+        and its position_value for the cross-section"""
+        return self.boundaries.positive.maximum_curvature.compute(strain_position)
 
-    def _minimum_positive_curvature(self, strain: float, position: float) -> float:
-        return self.boundaries.positive.minimum_curvature.compute(strain, position)
+    def _minimum_positive_curvature(self, strain_position: StrainPosition) -> float:
+        """compute the minimum possible positive curvature given the strain_value
+        and its position_value for the cross-section"""
+        return self.boundaries.positive.minimum_curvature.compute(strain_position)
 
-    def _maximum_negative_curvature(self, strain: float, position: float) -> float:
-        return self.boundaries.negative.maximum_curvature.compute(strain, position)
+    def _maximum_negative_curvature(self, strain_position: StrainPosition) -> float:
+        """compute the maximum possible negative curvature given the strain_value
+        and its position_value for the cross-section"""
+        return self.boundaries.negative.maximum_curvature.compute(strain_position)
 
-    def _minimum_negative_curvature(self, strain: float, position: float) -> float:
-        return self.boundaries.negative.minimum_curvature.compute(strain, position)
+    def _minimum_negative_curvature(self, strain_position: StrainPosition) -> float:
+        """compute the minimum possible negative curvature given the strain_value
+        and its position_value for the cross-section"""
+        return self.boundaries.negative.minimum_curvature.compute(strain_position)
 
     def _compute_positive_curvature_values(self) -> None:
         if self.positive is not None:
@@ -418,7 +417,7 @@ class MKappaCurve:
             self._compute_positive_curvature_intermediate()
 
     def _compute_values(
-        self, m_kappa: MKappaByStrainPosition, material: str = None
+        self, m_kappa: MKappaByStrainPosition
     ) -> None:
         if m_kappa.successful:
             self._save_values(
@@ -426,25 +425,27 @@ class MKappaCurve:
                 m_kappa.curvature,
                 m_kappa.neutral_axis,
                 m_kappa.computed_cross_section,
-                m_kappa.strain_at_position,
                 m_kappa.strain_position,
-                material,
             )
+        else:
+            print(f'============\n'
+                  f'not successful:\n'
+                  f'position_value={m_kappa.strain_position}')
+            print(m_kappa._print_initialization())
+            print(m_kappa._print_iterations())
 
     def _insert_zero(self) -> None:
-        self._save_values(0.0, 0.0, 0.0, None, 0.0, 0.0, "-")
+        self._save_values(0.0, 0.0, 0.0, None, None)
 
     def _m_kappa(
         self,
-        position_strain: float,
-        strain_at_position: float,
+        position_strain: StrainPosition,
         maximum_curvature: float,
         minimum_curvature: float,
     ) -> MKappaByStrainPosition:
         return MKappaByStrainPosition(
             self.cross_section,
             strain_position=position_strain,
-            strain_at_position=strain_at_position,
             maximum_curvature=maximum_curvature,
             minimum_curvature=minimum_curvature,
         )
@@ -455,18 +456,14 @@ class MKappaCurve:
         curvature: float,
         neutral_axis: float,
         computed_cross_section,
-        strain: float = None,
-        position: float = None,
-        material: str = None,
+        strain_position: StrainPosition = None,
     ) -> None:
         self._m_kappa_points.add(
             moment,
             curvature,
             neutral_axis,
             computed_cross_section,
-            strain,
-            position,
-            material,
+            strain_position
         )
 
     def __sort_m_kappa_by_curvature(self) -> None:
