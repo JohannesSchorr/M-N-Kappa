3997225dfc7aba8f6a5b3f2dfcc971d079635b39
Johannes Schorr <johannes.schorr@ke.uni-stuttgart.de>
2022-11-19 13:40:42 +0100

    [update] geometry.py
    - Class Circle: add properties 'top_edge' and 'bottom_edge'
    - add following Classes of composed Geometries:
      - ComposedGeometry
      - IProfile
      - RebarLayer
      - UPEProfile

--- m_n_kappa/geometry.py
+++ m_n_kappa/geometry.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 
 from .general import print_sections, str_start_end
 from .material import Material
 from .section import Section
+from .crosssection import Crosssection
 
 """
 Geometries
@@ -22,10 +24,13 @@ class Geometry(ABC):
 
     """basic geometry class"""
 
-    def __add__(self, other: Material):
+    def __add__(self, other: Material) -> Section:
         return self._build_section(other)
 
-    def __radd__(self, other: Material):
+    def __radd__(self, other: Material) -> Section:
+        return self._build_section(other)
+
+    def __mul__(self, other: Material) -> Section:
         return self._build_section(other)
 
     def _build_section(self, other: Material) -> Section:
@@ -194,6 +199,14 @@ class Circle(Geometry):
         return [self.centroid]
 
     @property
+    def top_edge(self):
+        return self.centroid - 0.5 * self.diameter
+
+    @property
+    def bottom_edge(self):
+        return self.centroid + 0.5 * self.diameter
+
+    @property
     def height(self) -> float:
         return 0.0
 
@@ -320,3 +333,198 @@ class Trapezoid(Geometry):
     @property
     def width_interception(self) -> float:
         return self.top_width - self.top_edge * self.width_slope
+
+
+class ComposedGeometry(ABC):
+
+    """
+    Geometry consisting of several basic geometries
+
+    such as:
+    - RebarLayer: consists of several circular geometries
+    - profile: T-profile consists of two flanges and one web
+    """
+
+    def __add__(self, other: Material) -> Crosssection:
+        return self._build_section(other)
+
+    def __radd__(self, other: Material) -> Crosssection:
+        return self._build_section(other)
+
+    def __mul__(self, other) -> Crosssection:
+        return self._build_section(other)
+
+    def _build_section(self, other: Material) -> Crosssection:
+        if isinstance(other, Material):
+            sections = [
+                Section(geometry=geometry, material=other)
+                for geometry in self.geometries
+            ]
+            return Crosssection(sections)
+        else:
+            raise TypeError(
+                f'unsupported operand type(s) for +: "{type(self)}" and "{type(other)}"'
+            )
+
+    @property
+    @abstractmethod
+    def geometries(self) -> list[Geometry]:
+        ...
+
+
+@dataclass
+class IProfile(ComposedGeometry):
+
+    """
+    I-Profile composed of class Rectangles
+
+    Parameters
+    ----------
+    top_edge: float
+        top_edge of the I-profile
+    t_w: float
+        web-thickness of the I-profile
+    h_w: float
+        web-height of the I-profile
+    t_fo: float = None
+        thickness of the top-flange
+    b_fo: float = None
+        width of the top-flange
+    t_fu: float = None
+        thickness of the bottom-flange
+    b_fu: float = None
+        width of the bottom-flange
+    has_top_flange: bool = True
+        decide if I-profile has a top-flange (Default: True).
+        If False: no top-flange is considered
+    has_bottom_flange: bool = True
+        decide if I-profile has a bottom-flange (Default: True)
+        if False: no top-flange is considered
+
+    Example(s):
+    -----------
+    - HEB 200 -> IProfile(top_edge=0., t_fo=15.5, b_fo=200.0, t_w=9.5, h_w=169.0)
+    """
+
+    top_edge: float
+    t_w: float
+    h_w: float
+    t_fo: float = None
+    b_fo: float = None
+    t_fu: float = None
+    b_fu: float = None
+    has_top_flange: bool = True
+    has_bottom_flange: bool = True
+    geometries: list = None
+
+    def __post_init__(self):
+        self.geometries = []
+        if self.has_bottom_flange and self.t_fu is None and self.t_fo is not None:
+            self.t_fu = self.t_fo
+        if self.has_bottom_flange and self.b_fu is None and self.b_fo is not None:
+            self.b_fu = self.b_fo
+        if self.has_top_flange and self.t_fo is not None and self.b_fo is not None:
+            self.geometries.append(
+                Rectangle(self.top_edge, self.top_edge + self.t_fo, self.b_fo)
+            )
+        self.geometries.append(
+            Rectangle(
+                self.top_edge + self.t_fo,
+                self.top_edge + self.t_fo + self.h_w,
+                self.t_w,
+            )
+        )
+        if self.has_bottom_flange and self.t_fu is not None and self.b_fu is not None:
+            self.geometries.append(
+                Rectangle(
+                    self.top_edge + self.t_fo + self.h_w,
+                    self.top_edge + self.t_fo + self.h_w + self.t_fu,
+                    self.b_fu,
+                )
+            )
+
+
+@dataclass
+class RebarLayer(ComposedGeometry):
+
+    """
+    rebar-layer composed of several reinforcement-bars of class Circle
+
+    Parameters
+    ----------
+    rebar_diameter: float
+        diameter of rebars in the layer
+    centroid: float
+        position of the centroid in vertical direction
+    rebar_number: int = None
+        number of rebars within the layer (Alternative to argument 'width')
+    width: float = None
+        width of the rebar-layer (together with 'rebar_horizontal_distance' alternative to argument 'rebar_number').
+        In case 'rebar_number' is defined, the argument 'width' as well as 'rebar_horizontal_distance' value is ignored.
+    rebar_horizontal_distance : float
+        distance between the rebars in horizontal direction (Default: None).
+        See description in argument 'width'.
+
+    Example
+    -------
+    - RebarLayer(rebar_diameter: 12.0, centroid=10.0, rebar_number=10)
+      Creates 10 circles with diameter 12 and a vertical position of 10
+    """
+
+    rebar_diameter: float
+    centroid: float
+    rebar_number: int = None
+    width: float = None
+    rebar_horizontal_distance: float = None
+    geometries: list = None
+
+    def __post_init__(self):
+        if self.rebar_number is None and (
+            self.width is None or self.rebar_horizontal_distance is None
+        ):
+            raise ValueError(
+                "Neither argument 'rebar_number' or 'width' and "
+                "'rebar_horizontal_distance' must be defined"
+            )
+        if self.rebar_number is None:
+            self.rebar_number = int(self.width / self.rebar_horizontal_distance)
+        self.geometries = []
+        for _ in range(self.rebar_number):
+            self.geometries.append(Circle(self.rebar_diameter, self.centroid))
+
+
+@dataclass
+class UPEProfile(ComposedGeometry):
+
+    """
+    UPE-Profile composed of class Rectangles
+
+    Parameters
+    ----------
+    top_edge :
+
+    Example
+    -------
+    - UPE 200:
+    """
+
+    top_edge: float
+    t_f: float
+    b_f: float
+    t_w: float
+    h_w: float = None
+    h: float = None
+    geometries: list = None
+
+    def __post_init__(self):
+        if self.h_w is None and self.h is None:
+            raise ValueError(
+                'neither argument "h_w" (web-height) or "h" (profile-height) must be defined'
+            )
+        if self.h_w is None:
+            self.h_w = self.h - 2.0 * self.t_f
+        self.geometries = [
+            Rectangle(self.top_edge, self.top_edge + self.b_f, self.t_f),
+            Rectangle(self.top_edge, self.top_edge + self.t_w, self.h_w),
+            Rectangle(self.top_edge, self.top_edge + self.b_f, self.t_f),
+        ]
