6814aadd32bde15868495c0c28fa7b8a68b92268
Johannes Schorr <johannes.schorr@ke.uni-stuttgart.de>
2022-12-13 12:45:46 +0100

    [update] function.py
    overall
    - add missing/improve type-hints

--- m_n_kappa/function.py
+++ m_n_kappa/function.py
@@ -38,13 +38,13 @@ class General(Function):
         self._y = self._y_values()
         self._set_constants()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{self.__class__.__name__}(" \
                f"data=data[[variable, target]], " \
                f"variable={self.variable}, target={self.target})"
 
     @str_start_end
-    def __str__(self):
+    def __str__(self) -> str:
         text = [
             self._print_title(),
             self._print_initialization(),
@@ -112,16 +112,16 @@ class General(Function):
     def get_integration(self):
         return self.integration
 
-    def integration(self, variable_value: float):
+    def integration(self, variable_value: float) -> float:
         pass
 
-    def _function_repr(self):
+    def _function_repr(self) -> str:
         return ""
 
-    def _derivate_repr(self):
+    def _derivate_repr(self) -> str:
         return ""
 
-    def _integration_repr(self):
+    def _integration_repr(self) -> str:
         return ""
 
     def _set_constants(self):
@@ -142,49 +142,49 @@ class Linear(General):
     __slots__ = "_intersection", "_slope", "_data", "_variable", "_target", "_x", "_y"
 
     @property
-    def intersection(self):
+    def intersection(self) -> float:
         return self._intersection
 
     @property
-    def slope(self):
+    def slope(self) -> float:
         return self._slope
 
-    def derivate(self, variable_value: float):
+    def derivate(self, variable_value: float) -> float:
         return self.slope
 
-    def function(self, variable_value: float):
+    def function(self, variable_value: float) -> float:
         return self.slope * variable_value + self.intersection
 
-    def integration(self, variable_value: float, constant: float = 0.0):
+    def integration(self, variable_value: float, constant: float = 0.0) -> float:
         return (
             (1.0 / 2.0) * self.slope * variable_value ** 2.0
             + self.intersection * variable_value
             + constant
         )
 
-    def _base(self):
+    def _base(self) -> float:
         return self._x[0] - self._x[1]
 
-    def _numerator(self):
+    def _numerator(self) -> float:
         return self._y[0] - self._y[1]
 
-    def _derivate_repr(self):
-        return "{:.2f}".format(self.slope)
+    def _derivate_repr(self) -> str:
+        return f"{self.slope:.2f}"
 
-    def _function_repr(self):
-        return "{:.2f} x + {:.2f}".format(self.slope, self.intersection)
+    def _function_repr(self) -> str:
+        return f"{self.slope:.2f} x + {self.intersection:.2f}"
 
-    def _integration_repr(self):
-        return "1/2 * {:.2f} x^2 + {:.2f} x".format(self.slope, self.intersection)
+    def _integration_repr(self) -> str:
+        return f"1/2 * {self.slope:.2f} x^2 + {self.intersection:.2f} x"
 
-    def _set_constants(self):
+    def _set_constants(self) -> None:
         self.__set_slope()
         self.__set_intersection()
 
-    def __set_slope(self):
+    def __set_slope(self) -> None:
         self._slope = self._numerator() / self._base()
 
-    def __set_intersection(self):
+    def __set_intersection(self) -> None:
         self._intersection = self._y[0] - self._x[0] * self.slope
 
 
@@ -194,24 +194,24 @@ class Polynominal(General):
     __slots__ = "_a", "_b", "_c", "_data", "_variable", "_target", "_x", "_y"
 
     @property
-    def a(self):
+    def a(self) -> float:
         return self._a
 
     @property
-    def b(self):
+    def b(self) -> float:
         return self._b
 
     @property
-    def c(self):
+    def c(self) -> float:
         return self._c
 
-    def derivate(self, variable_value: float):
+    def derivate(self, variable_value: float) -> float:
         return 2.0 * self.a * variable_value + self.b
 
-    def function(self, variable_value: float):
+    def function(self, variable_value: float) -> float:
         return self.a * variable_value ** 2.0 + self.b * variable_value + self.c
 
-    def integration(self, variable_value: float, constant: float = 0.0):
+    def integration(self, variable_value: float, constant: float = 0.0) -> float:
         return (
             (1.0 / 3.0) * self.a * variable_value ** 3.0
             + (1.0 / 2.0) * self.b * variable_value ** 2.0
@@ -219,33 +219,33 @@ class Polynominal(General):
             + constant
         )
 
-    def _derivate_repr(self):
-        return "2 {:.2f} x + {:.2f}".format(self.a, self.b)
+    def _derivate_repr(self) -> str:
+        return f"2 {self.a:.2f} x + {self.b:.2f}"
 
-    def _function_repr(self):
-        return "{:.2f} x^2 + {:.2f} x + {:.2f}".format(self.a, self.b, self.c)
+    def _function_repr(self) -> str:
+        return f"{self.a:.2f} x^2 + {self.b:.2f} x + {self.c:.2f}"
 
-    def _integration_repr(self):
-        return "(1/3) {:.2f} x^3 + (1/2) {:.2f} x^2 + {:.2f} x".format(
-            self.a, self.b, self.c
-        )
+    def _integration_repr(self) -> str:
+        return f"(1/3) {self.a:.2f} x^3 + (1/2) {self.b:.2f} x^2 + {self.c:.2f} x"
 
-    def _set_constants(self):
+    def _set_constants(self) -> None:
         self.__set_a()
         self.__set_b()
         self.__set_c()
 
-    def __set_a(self):
+    def __set_a(self) -> None:
+        if self.x[0] == self.x[1] or self.x[0] == self.x[2] or self.x[1] == self.x[2]:
+            raise ZeroDivisionError(self.data)
         self._a = (self.y[2] - self.y[0]) / (
             (self.x[2] - self.x[0]) * (self.x[2] - self.x[1])
         ) - (self.y[1] - self.y[0]) / (
             (self.x[1] - self.x[0]) * (self.x[2] - self.x[1])
         )
 
-    def __set_b(self):
+    def __set_b(self) -> None:
         self._b = ((self.y[1] - self.y[0]) / (self.x[1] - self.x[0])) - (
             self.x[1] + self.x[0]
         ) * self.a
 
-    def __set_c(self):
+    def __set_c(self) -> None:
         self._c = self.y[0] - self.x[0] ** 2.0 * self.a - self.x[0] * self.b
