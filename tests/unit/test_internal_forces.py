011d0f5f3b8f4e8a88f6ca6677b93cd28272919d
Johannes Schorr <johannes.schorr@ke.uni-stuttgart.de>
2022-12-13 15:11:19 +0100

    [update] test_internal_forces.py
    added Test-cases:
    - "TestSingleSpanSingleLoads" to consider single span with more than one single load

--- tests/unit/test_internal_forces.py
+++ tests/unit/test_internal_forces.py
@@ -85,7 +85,38 @@ class TestSingleSpanSingleLoad(TestCase):
         )
 
     def test_position_of_maximum_moment(self):
-        self.assertEqual(self.forces.position_of_maximum_moment(), self.load_position)
+        self.assertEqual(self.forces.positions_of_maximum_moment(), [self.load_position])
+
+    def test_position_of_maximum_deformation(self):
+        self.assertEqual(self.forces.position_of_maximum_deformation(), self.load_position)
+
+
+class TestSingleSpanSingleLoads(TestCase):
+    """Single span with more than one single load"""
+    def setUp(self) -> None:
+        self.beam_length = 10.0
+        self.load = 10.0
+        self.load_positions = [1./3.*self.beam_length, 2./3.*self.beam_length]
+        self.single_loads = [SingleLoad(position, 0.5*self.load) for position in self.load_positions]
+        self.forces = SingleSpan(length=self.beam_length, loads=self.single_loads)
+
+    def test_transversal_shear_support_left(self):
+        self.assertAlmostEqual(self.forces.transversal_shear_support_left, 0.5 * self.load)
+
+    def test_transversal_shear_support_right(self):
+        self.assertAlmostEqual(self.forces.transversal_shear_support_right, -0.5 * self.load)
+
+    def test_length(self):
+        self.assertEqual(self.forces.length, self.beam_length)
+
+    def test_load(self):
+        self.assertEqual(self.forces.loading, self.load)
+
+    def test_positions_of_maximum_moment(self):
+        self.assertListEqual(self.forces.positions_of_maximum_moment(), self.load_positions)
+
+    def test_position_of_maximum_deformation(self):
+        self.assertEqual(self.forces.position_of_maximum_deformation(), 0.5*self.beam_length)
 
 
 if __name__ == "__main__":
