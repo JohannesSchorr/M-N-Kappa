3e8a044d11d041ad198a6b94d25efc6d47df8279
Johannes Schorr <johannes.schorr@ke.uni-stuttgart.de>
2022-12-25 19:13:41 +0100

    [update] __init__.py
    added classes:
    - Material: for creation of custom materials
    - Geometry: for creation of custom geometries
    - Node: for better analysis

--- m_n_kappa/__init__.py
+++ m_n_kappa/__init__.py
@@ -1,8 +1,8 @@
-from .material import Concrete, Steel, Reinforcement
-from .geometry import Rectangle, Trapezoid, UPEProfile, RebarLayer, IProfile, Circle
+from .material import Material, Concrete, Steel, Reinforcement
+from .geometry import Geometry, Rectangle, Trapezoid, UPEProfile, RebarLayer, IProfile, Circle
 from .section import Section
 from .crosssection import Crosssection
-from .deformation import Beam
+from .deformation import Beam, Node
 from .internalforces import (
     SingleSpanUniformLoad,
     SingleLoad,
