f1bd1c6c765d3cfdf557ecf2fff86a8a7ce6c0c7
Johannes Schorr <johannes.schorr@ke.uni-stuttgart.de>
2023-01-07 22:24:19 +0100

    [update] copyright and version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,9 +7,9 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'M-N-Kappa'
-copyright = '2022, Johannes Schorr'
+copyright = '2023, Johannes Schorr'
 author = 'Johannes Schorr'
-release = 'v2022.12.0'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
