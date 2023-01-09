259cab8b96e23c9c0493c0eedc21e839b6139f70
Johannes Schorr <johannes.schorr@ke.uni-stuttgart.de>
2023-01-09 09:02:52 +0100

    [update] class Material - documentation
    follow the recommendations given by doc/source/contributing.rst

--- m_n_kappa/material.py
+++ m_n_kappa/material.py
@@ -49,6 +49,8 @@ class Material:
     """
     Provides basic functionality for materials
 
+    .. versionadded: 0.1.0
+
     In case custom-type materials are created these must inherit from this class.
     """
 
@@ -63,6 +65,50 @@ class Material:
             Possible values are:
             - slab
             - girder
+
+        Examples
+        --------
+
+        **Base class**
+
+        All material-models needs inheritance from :py:class:`~m_n_kappa.material.Material` to achieve basic functionality.
+
+        Stress-strain-points in the stress-strain-relationships need to be defined by :py:class:`m_n_kappa.material.StressStrain`.
+
+        **Adding new materials**
+
+        A new material must inherit from :py:class:`~m_n_kappa.material.Material`.
+
+        The following code implements an arbitrary material model that act linear-elastic under compression and ideal-plastic
+        under tension loading.
+
+        Two class-properties must be defined after initialization of the material:
+
+        1. ``stress_strain`` (``_stress_strain`` at initialization): stress-strain-relationship as a list of :py:class:`~m_n_kappa.material.StressStrain`
+        2. ``section_type``: defining the section-type this material is applied to. Possible values are:
+
+           - ``'girder'``
+           - ``'slab'``
+
+        .. testcode::
+
+            from m_n_kappa.material import Material, StressStrain
+
+            class Arbitrary(Material):
+
+            \"""arbitrary material\"""
+
+            def __init__(self):
+                self._stress_strain = [
+                    StressStrain(stress=-10.0, strain=-0.001),
+                    StressStrain(stress=0.0, strain=0.0),
+                    StressStrain(stress=10.0, strain=0.001),
+                    StressStrain(stress=10.0, strain=0.01),
+                ]
+
+            @property
+            def section_type(self):
+                return "girder"
         """
         self._stress_strain = stress_strain
         self._section_type = section_type
