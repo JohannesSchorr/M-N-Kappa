a3f82c876661efa3cf4352feba9c698f68c2738b
Johannes Schorr <johannes.schorr@ke.uni-stuttgart.de>
2023-01-20 13:19:30 +0100

    [add] logging on module level
    for better maintainability

--- m_n_kappa/points.py
+++ m_n_kappa/points.py
@@ -1,4 +1,12 @@
 from dataclasses import dataclass
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.WARN)
+logger.addHandler(stream_handler)
 
 from .general import (
     print_sections,
