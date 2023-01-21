06485a3703fd5d194a2185a38a469000a0511587
Johannes Schorr <johannes.schorr@ke.uni-stuttgart.de>
2023-01-21 11:36:20 +0100

    [add] logging using config-file
    see 'logging-config.yaml'

--- m_n_kappa/crosssection.py
+++ m_n_kappa/crosssection.py
@@ -1,14 +1,3 @@
-from dataclasses import dataclass
-import logging
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.WARN)
-logger.addHandler(stream_handler)
-
-
 from .general import (
     str_start_end,
     print_chapter,
@@ -32,6 +21,17 @@ from .curvature_boundaries import (
     Boundaries,
     BoundaryValues,
 )
+
+import logging
+import logging.config
+import yaml
+import pathlib
+
+with open(pathlib.Path(__file__).parent.absolute() / "logging-config.yaml", 'r') as f:
+    config = yaml.safe_load(f.read())
+    logging.config.dictConfig(config)
+
+logger = logging.getLogger(__name__)
 
 
 def axial_force(sections: list[ComputationSection]) -> float:
