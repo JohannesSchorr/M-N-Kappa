6b92997cd948e87513e9cb01e78420ba7c11d001
Johannes Schorr <johannes.schorr@ke.uni-stuttgart.de>
2023-02-03 12:11:39 +0100

    [add] class 'MKappaAtAxialForce' - logging

--- m_n_kappa/curves_m_n_kappa.py
+++ m_n_kappa/curves_m_n_kappa.py
@@ -930,6 +930,7 @@ class MKappaAtAxialForce:
             2.
     """
 
+    @logs_init
     def __init__(self, cross_section: Crosssection, axial_force: float):
         self._cross_section = cross_section
         self._axial_force = axial_force
