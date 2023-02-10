72786f5256fca21d134779ee761e4b2017cb37d9
Johannes Schorr <johannes.schorr@ke.uni-stuttgart.de>
2023-02-10 12:18:27 +0100

    [add] class 'MNByStrain'
    for computation of a uniform strain under an applied axial-force

--- m_n_kappa/__init__.py
+++ m_n_kappa/__init__.py
@@ -3,7 +3,7 @@ from .material import Material, Concrete, Steel, Reinforcement
 from .geometry import Geometry, Rectangle, Trapezoid, UPEProfile, RebarLayer, IProfile, Circle
 from .section import Section
 from .crosssection import Crosssection
-from .points import MKappaByStrainPosition, MKappaByConstantCurvature
+from .points import MKappaByStrainPosition, MKappaByConstantCurvature, MNByStrain
 from .curves_m_kappa import MKappaCurve
 from .deformation import Beam, Node
 from .loading import (
