06a48a50c7f80cab1f9d89d174c8ab473fc44748
Johannes Schorr <johannes.schorr@ke.uni-stuttgart.de>
2023-02-28 15:45:43 +0100

    [update] blackify and add MNKappaCurve

--- m_n_kappa/__init__.py
+++ m_n_kappa/__init__.py
@@ -1,11 +1,19 @@
 from .general import EffectiveWidths, StrainPosition
 from .material import Material, Concrete, Steel, Reinforcement
-from .geometry import Geometry, Rectangle, Trapezoid, UPEProfile, RebarLayer, IProfile, Circle
+from .geometry import (
+    Geometry,
+    Rectangle,
+    Trapezoid,
+    UPEProfile,
+    RebarLayer,
+    IProfile,
+    Circle,
+)
 from .section import Section
 from .crosssection import Crosssection
 from .points import MKappaByStrainPosition, MKappaByConstantCurvature, MomentAxialForce
 from .curves_m_kappa import MKappaCurve
-from .curves_m_n_kappa import MNCurve
+from .curves_m_n_kappa import MNCurve, MNKappaCurve
 from .deformation import Beam, Node
 from .loading import (
     SingleSpanUniformLoad,
