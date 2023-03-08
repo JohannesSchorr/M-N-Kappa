b6020c14c7446b61e99115347017c66ee8fc4387
Johannes Schorr <johannes.schorr@ke.uni-stuttgart.de>
2023-03-08 16:16:50 +0100

    [update] blackify

--- tests/unit/test_internal_forces.py
+++ tests/unit/test_internal_forces.py
@@ -12,12 +12,12 @@ class TestSingleSpanUniformLoad(TestCase):
     def test_moment(self):
         self.assertEqual(
             self.forces.moment(0.5 * self.beam_length),
-            self.load * self.beam_length ** 2.0 / 8.0,
+            self.load * self.beam_length**2.0 / 8.0,
         )
 
     def test_maximum_moment(self):
         self.assertEqual(
-            self.forces.maximum_moment, self.load * self.beam_length ** 2.0 / 8.0
+            self.forces.maximum_moment, self.load * self.beam_length**2.0 / 8.0
         )
 
     def test_transversal_shear_1(self):
@@ -85,26 +85,40 @@ class TestSingleSpanSingleLoad(TestCase):
         )
 
     def test_position_of_maximum_moment(self):
-        self.assertEqual(self.forces.positions_of_maximum_moment(), [self.load_position])
+        self.assertEqual(
+            self.forces.positions_of_maximum_moment(), [self.load_position]
+        )
 
     def test_position_of_maximum_deformation(self):
-        self.assertEqual(self.forces.position_of_maximum_deformation(), self.load_position)
+        self.assertEqual(
+            self.forces.position_of_maximum_deformation(), self.load_position
+        )
 
 
 class TestSingleSpanSingleLoads(TestCase):
     """Single span with more than one single load"""
+
     def setUp(self) -> None:
         self.beam_length = 10.0
         self.load = 10.0
-        self.load_positions = [1./3.*self.beam_length, 2./3.*self.beam_length]
-        self.single_loads = [SingleLoad(position, 0.5*self.load) for position in self.load_positions]
+        self.load_positions = [
+            1.0 / 3.0 * self.beam_length,
+            2.0 / 3.0 * self.beam_length,
+        ]
+        self.single_loads = [
+            SingleLoad(position, 0.5 * self.load) for position in self.load_positions
+        ]
         self.forces = SingleSpan(length=self.beam_length, loads=self.single_loads)
 
     def test_transversal_shear_support_left(self):
-        self.assertAlmostEqual(self.forces.transversal_shear_support_left, 0.5 * self.load)
+        self.assertAlmostEqual(
+            self.forces.transversal_shear_support_left, 0.5 * self.load
+        )
 
     def test_transversal_shear_support_right(self):
-        self.assertAlmostEqual(self.forces.transversal_shear_support_right, -0.5 * self.load)
+        self.assertAlmostEqual(
+            self.forces.transversal_shear_support_right, -0.5 * self.load
+        )
 
     def test_length(self):
         self.assertEqual(self.forces.length, self.beam_length)
@@ -113,10 +127,14 @@ class TestSingleSpanSingleLoads(TestCase):
         self.assertEqual(self.forces.loading, self.load)
 
     def test_positions_of_maximum_moment(self):
-        self.assertListEqual(self.forces.positions_of_maximum_moment(), self.load_positions)
+        self.assertListEqual(
+            self.forces.positions_of_maximum_moment(), self.load_positions
+        )
 
     def test_position_of_maximum_deformation(self):
-        self.assertEqual(self.forces.position_of_maximum_deformation(), 0.5*self.beam_length)
+        self.assertEqual(
+            self.forces.position_of_maximum_deformation(), 0.5 * self.beam_length
+        )
 
 
 if __name__ == "__main__":
