779ae85f65b1dafba5da6cd8fce6e25a3f08cd28
Johannes Schorr <johannes.schorr@ke.uni-stuttgart.de>
2023-05-04 15:34:47 +0200

    [add] class 'CompositeNode' - add functionality regarding shear-connectors

--- m_n_kappa/node.py
+++ m_n_kappa/node.py
@@ -1,9 +1,11 @@
+import operator
 
 from .crosssection import Crosssection
 from .curves_m_kappa import MKappaCurvePoints, MKappaCurve
 from .curves_m_n_kappa import MNKappaCurvePoints, MNKappaCurve
+from .general import interpolation
 from .loading import ABCSingleSpan, SingleSpanSingleLoads
-from .shearconnector import ShearConnector
+from .shearconnector import ShearConnector, LoadSlip
 
 from .log import LoggerMethods
 
@@ -221,6 +223,13 @@ class CompositeNode(Node):
         """shear-connector(s) at this node"""
         return self._shear_connectors
 
+    def has_shear_connector(self) -> bool:
+        """informs if node has shear-connector"""
+        if self.shear_connectors:
+            return True
+        else:
+            return False
+
     def shear_force(self, slip: float) -> float:
         """
         compute the shear-force transferred by the shear-connectors at this position
@@ -236,6 +245,8 @@ class CompositeNode(Node):
             shear-force transferred by the shear-connectors at this position under
             the given slip
         """
+        if slip == 0:
+            return 0.0
         sign = slip / abs(slip)
         slip = abs(slip)
         return sign * sum(
@@ -262,3 +273,107 @@ class CompositeNode(Node):
             curvature by the moment and the axial_force
         """
         return self.curve_points.curvature(moment, axial_force)
+
+    def moment_by(self, axial_force: float, strain_difference: float) -> float:
+        """
+        determine the moment from the M-N-Kappa-Curve under given
+        ``axial_force`` and ``strain_difference``
+
+        Parameters
+        ----------
+        axial_force : float
+            axial-force
+        strain_difference : float
+            strain-difference
+
+        Returns
+        -------
+        float
+            moment from the M-N-Kappa-Curve considering the given ``axial_force`` and
+            ``strain_difference``
+        """
+        return self.curve_points.moment(
+            strain_difference=strain_difference, axial_force=axial_force
+        )
+
+    def incremental_deformation(
+        self,
+        load: ABCSingleSpan,
+        single_load: SingleSpanSingleLoads,
+        axial_force: float,
+    ) -> float:
+        """
+        determine incremental deformation at this node
+
+        for the deformation the curvature and single-load-moment at the node under given loading
+        are computed
+
+        Parameters
+        ----------
+        load : :py:class:`~m_n_kappa.loading.ABCSingleSpan`
+           load applied to the beam
+        single_load : :py:class:`~m_n_kappa.SingleSpanSingleLoads`
+            single-load applied at the point, where deformation is computed
+
+        Returns
+        -------
+        float
+            product from curvature and moment of the single-load at this node
+        """
+        moment = load.moment(self.position)
+        curvature = self.curvature_by(moment, axial_force)
+        single_load_moment = single_load.moment(self.position)
+        return curvature * single_load_moment
+
+    def max_axial_force(self) -> float:
+        return self.curve_points.max("axial_force")
+
+    def min_axial_force(self) -> float:
+        return self.curve_points.min("axial_force")
+
+    def shear_connectors_load_slip(self) -> list[LoadSlip]:
+        """
+        merged load-slip relationship of the shear-connectors at this node
+
+        Returns
+        -------
+        list[LoadSlip]
+            joint load-slip relationship
+        """
+        slips = []
+        for shear_connector in self.shear_connectors:
+            slips += shear_connector.slips()
+        max_slip = min(self.shear_connectors, key=operator.attrgetter("s_max")).s_max
+        slips = list(set(filter(lambda x: x <= max_slip, slips)))
+        return [LoadSlip(self.shear_force(slip), slip) for slip in slips]
+
+    def maximum_slip(self) -> float:
+        """
+        determine the maximum possible slip at this node
+
+        Assumes this is the first or the last node in the beam that
+        transfers shear-load.
+        Considers the maximum axial-force of the sub-cross-sections
+        and the maximum possible shear-load of the shear-connectors.
+
+        Returns
+        -------
+        float
+            maximum possible slip
+        """
+        beam_axial_forces = max(
+            abs(self.max_axial_force()), abs(self.min_axial_force())
+        )
+        load_slips = self.shear_connectors_load_slip()
+        max_shear_force = max(load_slips, key=operator.attrgetter("load")).load
+        axial_force = min([beam_axial_forces, max_shear_force])
+        for index in range(len(load_slips) - 2, -1, -1):
+            lower = load_slips[index]
+            upper = load_slips[index + 1]
+            if (
+                lower.load < axial_force <= upper.load
+                or upper.load <= axial_force < lower.load
+            ):
+                return interpolation(
+                    axial_force, lower.pair()[::-1], upper.pair()[::-1]
+                )
